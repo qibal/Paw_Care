a424aa1360ffa10f1929bbc3a7c85e22794356b6
qibal <iqbalherlambang.39@gmail.com>
2025-01-21 12:29:59 +0700

    Menu strip

--- PawCare/Form1.Designer.cs
+++ PawCare/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hewanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.peralatanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.hewanToolStripMenuItem,
+            this.peralatanToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // hewanToolStripMenuItem
+            // 
+            this.hewanToolStripMenuItem.Name = "hewanToolStripMenuItem";
+            this.hewanToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.hewanToolStripMenuItem.Text = "Hewan";
+            // 
+            // peralatanToolStripMenuItem
+            // 
+            this.peralatanToolStripMenuItem.Name = "peralatanToolStripMenuItem";
+            this.peralatanToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.peralatanToolStripMenuItem.Text = "Peralatan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hewanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem peralatanToolStripMenuItem;
     }
 }
 
